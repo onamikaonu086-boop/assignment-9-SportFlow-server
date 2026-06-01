76bd0475d00c6c0245fc748678020e8647e16a83
onamikaonu086-boop <onamikaonu086@gmail.com>
2026-06-02 01:49:07 +0600

    make the server again

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { toNodeHandler } from "better-auth/node";
 
 dotenv.config();
 
@@ -9,15 +12,9 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 const uri = process.env.MONGODB_URI;
 
-app.use(cors({
-  origin: "*", 
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
-app.use(express.json());
 
 if (!uri) {
-  console.error("❌ Error: MONGODB_URI is not defined in .env file!");
+  console.error("MONGODB_URI is not defined in .env file");
   process.exit(1);
 }
 
@@ -29,32 +26,31 @@ const client = new MongoClient(uri, {
   },
 });
 
+
+
 async function startServer() {
   try {
     await client.connect();
-    console.log("Connected to MongoDB!");
+    console.log("Connected to MongoDB");
 
-    const database = client.db("sportflowDB");
-    const facilityCollection = database.collection("facilities");
+    const db = client.db("sportflowDB");
+
+
 
     app.get("/", (req, res) => {
-      res.send("SportFlow Server is running perfectly!");
+      res.send("SportFlow Server is running!");
     });
 
-    app.post("/add-facility", async (req, res) => {
-      const facilityData = req.body;
-      console.log("Data received:", facilityData);
-      const result = await facilityCollection.insertOne(facilityData);
-      res.send(result);
-    });
 
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
 
+    
+   app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   } catch (error) {
     console.error("Failed to connect to MongoDB:", error);
   }
 }
+
 
 startServer();
